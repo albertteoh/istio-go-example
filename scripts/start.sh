e738aa8ac9a3d53b3d0e5c1d2d87c82052698a80
Albert <26584478+albertteoh@users.noreply.github.com>
2021-01-08 19:16:16 +1100

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 set -x
 
-istioctl install --set profile=demo -y -f tracing.yaml
+istioctl install --set profile=demo -y
 kubectl label namespace default istio-injection=enabled
 kubectl apply -f $DIR/jaeger-go-example.yaml
 sleep 10
